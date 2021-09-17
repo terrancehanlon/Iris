be91a2f35f0c39bc568de05782bb8729c5020119
terrancehanlon <hanlo053@umn.edu>
2021-09-16 22:02:42 -0700

    server

--- server.py
+++ server.py
@@ -23,8 +23,9 @@ import _thread
 from collections import defaultdict
 
 active_sockets = []
-zone_content = defaultdict(lambda: defaultdict(list))
-# zone_content['z1'] = {100 : ['1000,300,500','1000,400,500']}
+zone_content = defaultdict(lambda: defaultdict(lambda: defaultdict))
+# zone_content['z1'] = {100 : ['300,500','400,500']}
+# zone_content['z1']['id_of_client'] = []
 
 def on_new_client(clientsocket,addr):
     # print(addr, " has connected");
@@ -33,69 +34,80 @@ def on_new_client(clientsocket,addr):
         msg = clientsocket.recv(1024)
         # print(msg)
         msg = msg.decode('utf-8').split(';') # 0 idx is function to call, 1 idx is zone number
-        print(msg)
-        print(clientsocket)
+        # print(msg)
+        # print(clientsocket)
         # if msg != '':
         #     print(msg)
         
         if msg[0] == 'info':
             print(zone_content)
         
-        #init;zonename
-        #return current list of ids and locations
-        if msg[0] == 'init':
-            print("init getting locations")
-            locations = ''
-            for key,value in zone_content.items():
-                # print(value)
-                if key == msg[1]: #look for zone
-                    for key2, value2 in value.items():
-                        print(value2)
-                        locations = locations + (';'.join(value2))
-                        locations = locations + ';'
-            print(locations)
-            if locations == '':
-                clientsocket.sendall("empty".encode('utf-8'))
-            else:
-                clientsocket.sendall(locations.encode('utf-8'))                
-            print('sent locations')
+        # #init;zonename
+        # #return current list of ids and locations
+        # if msg[0] == 'init':
+        #     print("init getting locations")
+        #     locations = ''
+        #     for key,value in zone_content.items():
+        #         # print(value)
+        #         if key == msg[1]: #look for zone
+        #             for key2, value2 in value.items():
+        #                 print(value2)
+        #                 locations = locations + (';'.join(value2))
+        #                 locations = locations + ';'
+        #     print(locations)
+        #     if locations == '':
+        #         clientsocket.sendall("empty".encode('utf-8'))
+        #     else:
+        #         clientsocket.sendall(locations.encode('utf-8'))                
+        #     print('sent locations')
 
-        #updates the server with a clients location
-        #update;zonename;id_of_player;x;y
-        if msg[0] == 'updateServer':
-            good = True
-            msg.pop(0)
-            for x in msg:
-                if 'updateClient' in x:
-                    good = False
-            # print(msg[2:])
-            if good:
-                if zone_content[msg[0]][msg[1]] != msg[2:]:
-                    zone_content[msg[0]][msg[1]] = msg[2:]
-            clientsocket.sendall("recieved!".encode('utf-8'))
+        # #updates the server with a clients location
+        # #update;zonename;id_of_player;x;y
+        # if msg[0] == 'updateServer':
+        #     good = True
+        #     msg.pop(0)
+        #     for x in msg:
+        #         if 'updateClient' in x:
+        #             good = False
+        #     # print(msg[2:])
+        #     if good:
+        #         if zone_content[msg[0]][msg[1]] != msg[2:]:
+        #             zone_content[msg[0]][msg[1]] = msg[2:]
+        #     clientsocket.sendall("recieved!".encode('utf-8'))
 
         #updates the client with other clients locations 
         #check for id on client?
-        #updateClient;zonename;id_of_client
+        #updateClient;zonename;id_of_client;x;y
         elif msg[0] == 'updateClient':
             # print('updating client')
             locations = ''
+            # print(msg)
             zone = msg[1]
             id_of_client = msg[2]
-
+            #update clients location on server
+            zone_content[zone][id_of_client] = ';'.join(msg[slice(3,5)])
+            # print(zone_content)
+            temp_arr = []
             for key,value in zone_content.items():
                 if key == zone:
                     for key2, value2 in value.items():
+                        # print(key2)
+                        # print(value2)
                         if key2 != id_of_client:
-                            locations = locations + (';'.join(value2))
-            locations = locations + ';'
-            if locations == ';':
-                print('sending empty')
+                            temp = key2 + ';' + value2
+                            temp_arr.append(temp)
+                            # print(temp)
+                            # print(temp_arr)
+                            # locations = locations + (';'.join(value2))
+            # locations = locations + ';'
+            if temp_arr == []:
+                # print('sending empty')
                 clientsocket.sendall('empty'.encode('utf-8'))
             else:
-                print('locations not empty')
-                print(locations)
-                clientsocket.sendall(locations.encode('utf-8'))
+                # print('locations not empty')
+                # print(locations)
+                # print(temp_arr)
+                clientsocket.sendall(';'.join(temp_arr).encode('utf-8'))
             
 
     clientsocket.close()
